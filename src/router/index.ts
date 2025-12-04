6e3b17fd73878b7ca91eb8d97b14791344a1351b
crrashh1542 <1542@crrashh.com>
2025-12-04 17:09:01 +0800

    feat: 添加用户页路由
    Signed-off-by: crrashh1542 <1542@crrashh.com>

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,41 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import cookies from 'js-cookie'
+
+import setToast from '@/utils/setToast'
 import HomeView from '@/views/HomeView.vue'
 
-const routes = [
-    { path: '/', component: HomeView },
-
-    // 个人页面相关
-    { path: '/auth', component: () => import('@/views/AuthView.vue') },
-    {
-        path: '/user',
-        component: () => import('@/views/UserView.vue'),
-        meta: { requiresAuth: true },
-    },
-
-    // 比赛相关
-    { path: '/match', component: () => import('@/views/MatchListView.vue') },
-    { path: '/match/:id', component: () => import('@/views/MatchDetailView.vue') },
-]
-
-export default createRouter({
+const router = createRouter({
     history: createWebHistory(),
-    routes,
+    routes: [
+        { path: '/', component: HomeView },
+
+        // 个人页面相关
+        { path: '/auth', component: () => import('@/views/AuthView.vue') },
+        {
+            path: '/user',
+            component: () => import('@/views/UserView.vue'),
+            meta: { requiresAuth: true },
+        },
+        {
+            path: '/user/edit',
+            component: () => import('@/views/UserEditView.vue'),
+            // meta: { requiresAuth: true },
+        },
+
+        // 比赛相关
+        { path: '/match', component: () => import('@/views/MatchListView.vue') },
+        { path: '/match/:id', component: () => import('@/views/MatchDetailView.vue') },
+    ],
 })
+
+router.beforeEach((to, from, next) => {
+    const isAuthed = Boolean(cookies.get('token'))
+    if (to.meta.requiresAuth && !isAuthed) {
+        setToast('error', '未登录', '请先登录以访问该页面')
+        next('/auth')
+    } else {
+        next()
+    }
+})
+
+export default router
--- src/views/UserView.vue
+++ src/views/UserView.vue
@@ -1,19 +1,139 @@
 <script setup>
-import axios from 'axios'
 import { onMounted } from 'vue'
+import { useRouter, RouterLink } from 'vue-router'
 
-const apiUrl = import.meta.env.VITE_API_URL
+import { Button, Skeleton } from 'primevue'
 
-onMounted(() => {
+import setToast from '@/utils/setToast'
+import { initAuthStore } from '@/utils/initAuthStore'
+import { useAuthStore } from '@/stores/auth'
 
+const router = useRouter()
+const authStore = useAuthStore()
+
+onMounted(async () => {
+    // 如果因刷新等原因导致 authStore 未初始化，则先 init
+    if (!authStore.isReady) {
+        await initAuthStore()
+    }
+
+    if (!authStore.isAuthed) {
+        setToast('error', '获取用户信息失败', '当前尚未登录！')
+        router.push('/auth')
+    }
 })
 </script>
 
 <template>
+    <!-- 如果登录状态正常，就显示包含信息的 banner -->
+    <div class="banner" style="background-color: var(--p-emerald-50);" v-if="authStore.isAuthed">
+        <img :src="authStore.userInfo.avatar" alt="用户头像" srcset="" />
+        <div class="info">
+            <div class="title">{{ authStore.userInfo.username }}</div>
+            <div class="subtitle">
+                <span class="left">
+                    <div>{{ authStore.userInfo.name }}&nbsp;·&nbsp;{{ authStore.userInfo.role }}&nbsp;·&nbsp;{{
+                        authStore.userInfo.department }}</div>
+                    <div>
+                        <span class="pi pi-users">&nbsp;{{ authStore.userInfo.studentid }}</span>
+                    </div>
+                </span>
+                <span class="grow"></span>
+                <router-link to="/user/edit">
+                    <Button label="编辑个人信息" icon="pi pi-pencil" severity="secondary" />
+                </router-link>
+            </div>
+        </div>
+    </div>
+    <!-- 否则显示加载中骨架屏 -->
+    <div class="banner" style="background-color: var(--p-emerald-50);" v-else>
+        <Skeleton shape="circle" size="129px" /> <!-- 8rem + border -->
+        <div class="info">
+            <Skeleton class="title" width="12rem" height="2.25rem" />
+            <div class="subtitle skeleton">
+                <Skeleton width="20rem" height="1.25rem" />
+                <Skeleton width="20rem" height="1.25rem" />
+            </div>
+        </div>
+    </div>
+
     <main>
-        <h1>芝士用户</h1>
-        {{ }}
+        <div class="list">
+            测试信息
+        </div>
     </main>
 </template>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.banner {
+    display: flex;
+    padding: var(--e-banner-padding);
+    gap: 2rem;
+    flex-direction: var(--banner-direction);
+    align-items: center;
+
+    img {
+        width: 8rem;
+        height: 8rem;
+        border-radius: 50%;
+        padding: 2px;
+        border: 1px solid #ddd;
+        background-color: #fff;
+    }
+
+    .info {
+        flex: 1;
+
+        .title {
+            font-size: 2.25rem;
+            font-weight: bold;
+            margin: .5rem var(--banner-h-margin) 1rem;
+        }
+
+        .subtitle {
+            display: flex;
+            flex-direction: var(--banner-direction);
+            gap: .5rem;
+            align-items: center;
+            font-size: 1rem;
+            line-height: 2;
+            color: #555;
+
+            .pi {
+                font-family: 'primeicons', 'Inter';
+            }
+
+            .grow {
+                flex-grow: 1;
+            }
+
+            .p-button {
+                border: 1px solid #ccc !important;
+            }
+        }
+
+        .subtitle.skeleton {
+            display: flex;
+            flex-direction: column;
+            gap: .5rem;
+            align-items: start;
+        }
+    }
+
+    --banner-direction: row;
+    --banner-h-margin: 0;
+}
+
+main {
+    display: flex;
+    flex-direction: column;
+    gap: 3rem;
+}
+
+@media screen and (max-width: 800px) {
+    .banner {
+        --banner-direction: column;
+        --banner-h-margin: auto;
+    }
+}
+</style>
